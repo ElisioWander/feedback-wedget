373bb19bf78f9142e8cf347aff440f884be885bc
elisioWander <elisio741@hotmail.com>
2022-05-12 01:28:48 -0300

    resposivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Footer } from './Components/Footer'
 import { Header } from './Components/Header'
+import { Hero } from './Components/Hero'
 import { Main } from './Components/Main'
 import { Widget } from './Components/Widget'
 import { ScreenshotProvider } from './contexts/ScreenshotContext'
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -10,10 +10,17 @@ export function Header() {
 
   return (
     <header className="h-20 bg-zinc-400 dark:bg-zinc-800 text-zinc-100 flex items-center transition-colors " >
-      <div className="w-full flex items-center justify-between px-4 " >
+      <div className="w-full flex items-center justify-between px-4 md:px-16" >
         <span className="flex items-center" >
           <Switch onChange={handleToggleTheme} checked={theme == ''} onColor={"#3F3F46"} height={20} onHandleColor="#996DFF" />
         </span>
+
+        <div className="md:flex md:gap-6" >
+          <div className="md:w-24 md:h-4 bg-zinc-200 dark:bg-zinc-700 rounded-lg " ></div>
+          <div className="md:w-24 md:h-4 bg-zinc-200 dark:bg-zinc-700 rounded-lg " ></div>
+          <div className="md:w-24 md:h-4 bg-zinc-200 dark:bg-zinc-700 rounded-lg " ></div>
+          <div className="md:w-24 md:h-4 bg-zinc-200 dark:bg-zinc-700 rounded-lg " ></div>
+        </div>
 
         <div className="flex items-center gap-4" >
           <div className="w-8 h-8 bg-zinc-200 dark:bg-zinc-700 rounded-lg  transition-colors" ></div>
