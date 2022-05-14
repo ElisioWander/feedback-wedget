b5b1f5cb5a6c704d39e4b4bcb618e7ca15a55052
elisioWander <elisio741@hotmail.com>
2022-05-14 19:34:31 -0300

    axios-config

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-console.log(import.meta.env)
-
 //baseURL está referenciando a rota do back-end
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
